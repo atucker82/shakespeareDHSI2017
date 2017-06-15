3bed1cf8f4a055ca498821e50492fd4df9eda058
atucker82 <aarontucker82@gmail.com>
2017-06-15 16:00:09 -0700

    scene and act parser, all tests passed

--- src/Parser/ActParser.py
+++ src/Parser/ActParser.py
@@ -1,7 +1,43 @@
+import re
+
+from src.Parser.SceneParser import SceneParser
+from src.parts.parts import Act
+
+
 class ActParser(object):
 
-    def __init__(self, parent, line_of_text):
-        pass
+    ACT_HEADING_PATTERN = re.compile(
+        r"^ACT\s*(?P<act_number>[IVX]*)\."
+    )
+    ACT_AND_SCENE_HEADING_PATTERN = re.compile(
+        r"^ACT\s*(?P<act_number>[IV]+)\. SCENE (?P<scene_number>[1-9IXV]+)\."
+    )
 
-    def parse(selfself, text):
-        pass
+    def __init__(self, parent, line_of_text):
+        self.parent = parent
+        parsing_results = self.ACT_HEADING_PATTERN.match(line_of_text).groupdict()
+        self.act = Act(parsing_results["act_number"])
+
+    def parse(self, text):
+        if self.ACT_AND_SCENE_HEADING_PATTERN.match(text):
+            act_and_scene = self.ACT_AND_SCENE_HEADING_PATTERN.match(text).groupdict()
+            if act_and_scene["act_number"] == self.act.number:
+                scene_parser = SceneParser(
+                    self, "SCENE " + act_and_scene["scene_number"] + "."
+                )
+                return scene_parser
+            else:
+                return self.parent
+        elif SceneParser.SCENE_HEADING_PATTERN.match(text):
+            scene_parser = SceneParser(
+                self, text
+            )
+            return scene_parser
+        else:
+            return self.parent
+
+
+
+
+
+
--- tests/parser/ActParserTests.py
+++ tests/parser/ActParserTests.py
@@ -2,7 +2,7 @@ import unittest
 
 from src.Parser.ActParser import ActParser
 from src.Parser.PlayParser import PlayParser
-from src.Parser import SceneParser
+from src.Parser.SceneParser import SceneParser
 
 
 class ACtParserTests(unittest.TestCase):
@@ -13,6 +13,12 @@ class ACtParserTests(unittest.TestCase):
         self.assertEqual(act_parser.parent, play_parser)
         self.assertEqual(act_parser.act.number, "I")
 
+    def test_act_parser_recognizes_act_and_scene_heading(self):
+        play_parser = PlayParser(None, "THE REVENGE OF THE DHSI BUNNIES")
+        act_parser = ActParser(play_parser, "ACT I. SCENE I.")
+        next_parser = act_parser.parse("ACT I. SCENE I.")
+        self.assertIsInstance(next_parser, SceneParser)
+
     def test_create_act_parser_recognizes_new_scene(self):
         act_parser = ActParser(None, "ACT I.")
         scene_heading = "SCENE 1."
@@ -22,5 +28,6 @@ class ACtParserTests(unittest.TestCase):
     def test_create_act_parser_recognizes_end_of_scene(self):
         play_parser = PlayParser(None, "THE REVENGE OF THE DHSI BUNNIES")
         act_parser = ActParser(play_parser, "ACT I. SCENE III.")
-        next_parser = act_parser.parse("SCENE 1.")
-        self.assertIsInstance(next_parser, SceneParser)
+        next_parser = act_parser.parse("ACT II. SCENE 1.")
+        self.assertIsInstance(next_parser, PlayParser)
+        self.assertEqual(next_parser, play_parser)
